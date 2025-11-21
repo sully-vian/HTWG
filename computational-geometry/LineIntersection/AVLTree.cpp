0046ed2fa860a26b705e60caff7f70ef77fbe0e5
Vianney HERVY <vian.hervy@free.fr>
2025-11-21 18:33:25 +0100

    PA2: almost done with avl trees

--- computational-geometry/LineIntersection/AVLTree.cpp
+++ computational-geometry/LineIntersection/AVLTree.cpp
@@ -2,31 +2,101 @@
 #include <functional>
 #include <iostream>
 #include <sstream>
+#include <vector>
 
-template <typename K, typename V> class AVLNode {
+template <typename KeyType, typename ValueType> class Node {
   public:
-    AVLNode(K key, V value)
-        : height(0), key(key), value(value), left(), right() {};
+    Node(KeyType key, ValueType value)
+        : key(key), val(value), height(0), left(nullptr), right(nullptr),
+          next(nullptr) {};
 
-    // returns thenew root of this subtree
-    AVLNode *insert(K key, V value) {
-        if (key < this->key) {
-            if (left) {
-                left = left->insert(key, value);
-            } else {
-                left = new AVLNode(key, value);
-            }
-        } else if (key > this->key) {
-            if (right) {
-                right = right->insert(key, value);
-            } else {
-                right = new AVLNode(key, value);
-            }
-        } else { // duplicate key
-            return this;
+    static bool insert(Node *&root, KeyType key, ValueType v) {
+        Node *pred = nullptr;
+        Node *succ = nullptr;
+        return insertInternal(root, key, v, pred, succ);
+    }
+
+    static bool remove(Node *&root, KeyType key) {
+        return removeInternal(root, root, key);
+    }
+
+    static void range(
+        Node *root, KeyType lo, KeyType hi,
+        const std::function<void(const KeyType &, const ValueType &)> &visit) {
+        if (!root) {
+            return;
         }
-        updateHeight();
-        return balance();
+        Node *start = lowerBound(root, lo);
+        while (start && start->key < lo) {
+            start = start->next;
+        }
+        for (Node *n = start; n && n->key <= hi; n = n->next) {
+            visit(n->key, n->val);
+        }
+    }
+
+    static void search(void) {
+        // TODO
+    }
+
+    // TODO: finish
+    static bool validate(Node *&root) {
+        if (!root) {
+            return true;
+        }
+        bool ok = true;
+        std::vector<Node *> inOrder;
+
+        std::function<int(Node *)> dfs = [&](Node *n) -> int {
+            if (!n)
+                return -1;
+            int lh = dfs(n->left);
+            inOrder.push_back(n);
+            int rh = dfs(n->right);
+
+            // check height
+            int expected = std::max(lh, rh) + 1;
+            if (n->height != expected) {
+                ok = false;
+            }
+
+            // check balance
+            int bf = rh - lh;
+            if (bf < -1 || bf > 1) {
+                ok = false;
+            }
+
+            // check local ordering
+            if (n->left) {
+                if (n->left->key >= n->key) {
+                    ok = false;
+                }
+            }
+            if (n->right) {
+                if (n->right->key <= n->key) {
+                    ok = false;
+                }
+            }
+            return expected;
+        };
+        dfs(root);
+
+        // check in-order strict increase and successor chain alignment
+        for (size_t i = 0; i < inOrder.size(); ++i) {
+            if (i + 1 < inOrder.size()) {
+                if (inOrder[i]->next != inOrder[i + 1]) {
+                    ok = false;
+                }
+                if (inOrder[i]->key >= inOrder[i + 1]->key) {
+                    ok = false;
+                }
+            } else {
+                if (inOrder[i]->next != nullptr) {
+                    ok = false;
+                }
+            }
+        }
+        return ok;
     }
 
     /*
@@ -36,7 +106,7 @@ template <typename K, typename V> class AVLNode {
         std::ostringstream oss;
         oss << "digraph G {\n";
 
-        std::function<void(AVLNode *)> traverse = [&](AVLNode *node) {
+        std::function<void(Node *)> traverse = [&](Node *node) {
             if (!node) {
                 return;
             }
@@ -44,15 +114,21 @@ template <typename K, typename V> class AVLNode {
             // Left child
             if (node->left) {
                 oss << "\t\"" << node->key << "\" -> \"" << node->left->key
-                    << "\";\n";
+                    << "\"[color=red, label=\"L\", fontcolor=red];\n";
                 traverse(node->left);
             }
 
             // Right child
             if (node->right) {
                 oss << "\t\"" << node->key << "\" -> \"" << node->right->key
-                    << "\";\n";
+                    << "\"[color=green, label=\"R\", fontcolor=green];\n";
                 traverse(node->right);
+            }
+
+            // successor
+            if (node->next) {
+                oss << "\t\"" << node->key << "\" -> \"" << node->next->key
+                    << "\"[style=dashed, color=blue];\n";
             }
         };
 
@@ -64,57 +140,185 @@ template <typename K, typename V> class AVLNode {
     void print(const char msg[]) { std::cout << msg << std::endl; }
 
   private:
-    K key;
-    V value;
+    KeyType key;
+    ValueType val;
     int height;
-    AVLNode *left;
-    AVLNode *right;
+    Node *left;
+    Node *right;
+    Node *next; // in-order successor
 
     /* ------------------- */
     /* Auxiliary functions */
     /* ------------------- */
 
-    int getHeight(AVLNode *node) { return node ? node->height : -1; }
-    int getBalance() { return getHeight(right) - getHeight(left); }
+    static int getHeight(const Node *p) { return p ? p->height : -1; }
 
-    void updateHeight() {
-        height = 1 + std::max(getHeight(left), getHeight(right));
+    static int getBalance(const Node *p) {
+        return p ? getHeight(p->right) - getHeight(p->left) : 0;
     }
 
-    AVLNode *rotateLeft() {
-        AVLNode *newRoot = right;
-        right = newRoot->left;
-        newRoot->left = this;
-        updateHeight();
-        newRoot->updateHeight();
-        return newRoot;
-    }
-    AVLNode *rotateRight() {
-        AVLNode *newRoot = left;
-        left = newRoot->right;
-        newRoot->right = this;
-        updateHeight();
-        newRoot->updateHeight();
-        return newRoot;
-    }
-
-    AVLNode *balance() {
-        int b = getBalance();
-        if (b == -2) { // left heavy
-            if (left->getBalance() <= 0) {
-                return rotateRight();
-            } else {
-                left = left->rotateLeft();
-                return rotateRight();
+    static bool insertInternal(Node *&p, KeyType key, ValueType v, Node *&pred,
+                               Node *&succ) {
+        bool inserted;
+        if (!p) { // key does not exist in the tree
+            p = new Node(key, v);
+            balance(p);
+            p->next = succ;
+            if (pred) {
+                pred->next = p;
             }
-        } else if (b == 2) { // right heavy
-            if (right->getBalance() >= 0) {
-                return rotateLeft();
-            } else {
-                right = right->rotateRight();
-                return rotateLeft();
+            inserted = true;
+        } else if (key < p->key) { // smaller, insert left
+            succ = p;
+            inserted = insertInternal(p->left, key, v, pred, succ);
+        } else if (key > p->key) { // larger, insert right
+            pred = p;
+            inserted = insertInternal(p->right, key, v, pred, succ);
+        } else {                   // key exists, nothing to do
+            inserted = false;
+        }
+
+        if (inserted) {
+            balance(p);
+        }
+        return inserted;
+    }
+
+    static Node *findPred(Node *root, KeyType key) {
+        Node *pred = nullptr;
+        while (root) {
+            if (key < root->key) {
+                root = root->left;
+            } else if (key > root->key) {
+                pred = root;
+                root = root->right;
+            } else { // found
+                break;
             }
         }
-        return this; // nothing to do
+        return pred;
+    }
+
+    static bool removeInternal(Node *&p, Node *root, KeyType key) {
+        bool removed = false;
+        if (!p) {
+            removed = false;
+
+        } else if (key < p->key) {
+            removed = removeInternal(p->left, root, key);
+
+        } else if (key > p->key) {
+            removed = removeInternal(p->right, root, key);
+
+        } else {                         // found node
+            Node *pred = findPred(root, key);
+            if (!p->left && !p->right) { // leaf
+                if (pred) {
+                    pred->next = p->next;
+                }
+                delete p;
+                p = nullptr;
+
+            } else if (!p->left) { // only right child
+                if (pred) {
+                    pred->next = p->next;
+                }
+                Node *tmp = p;
+                p = p->right;
+                delete tmp;
+
+            } else if (!p->right) { // only right children
+                if (pred) {
+                    pred->next = p->next;
+                }
+                Node *tmp = p;
+                p = p->left;
+                delete tmp;
+
+            } else { // two children
+                // find successor (left most in right tree)
+                Node *succ = p->right;
+                while (succ->left) {
+                    succ = succ->left;
+                }
+                p->key = succ->key;
+                p->val = succ->val;
+                p->next = succ->next;
+                // remove successor node from right tree
+                removeInternal(p->right, root, succ->key);
+            }
+            removed = true;
+        }
+
+        if (removed && p) {
+            balance(p);
+        }
+        return removed;
+    }
+
+    // Lower bound (first node with key >= k)
+    static Node *lowerBound(Node *root, KeyType k) {
+        Node *candidate = nullptr;
+        while (root) {
+            if (k <= root->key) {
+                candidate = root;
+                root = root->left;
+            } else {
+                root = root->right;
+            }
+        }
+        return candidate;
+    }
+
+    static void rotateRight(Node *&p) {
+        Node *q = p->left;
+        p->left = q->right;
+        q->right = p;
+        p->height = std::max(getHeight(p->left), getHeight(p->right)) + 1;
+        q->height = std::max(getHeight(q->left), getHeight(q->right)) + 1;
+        p = q;
+    }
+
+    static void rotateLeft(Node *&p) {
+        Node *q = p->right;
+        p->right = q->left;
+        q->left = p;
+        p->height = std::max(getHeight(p->left), getHeight(p->right)) + 1;
+        q->height = std::max(getHeight(q->left), getHeight(q->right)) + 1;
+        p = q;
+    }
+
+    static void rotateLeftRight(Node *&p) {
+        rotateLeft(p->left);
+        rotateRight(p);
+    }
+
+    static void rotateRightLeft(Node *&p) {
+        rotateRight(p->right);
+        rotateLeft(p);
+    }
+
+    static void balance(Node *&p) {
+        if (!p) {
+            return;
+        }
+
+        // update height
+        p->height = std::max(getHeight(p->left), getHeight(p->right)) + 1;
+
+        int b = getBalance(p);
+        if (b == -2) { // left heavy
+            if (getBalance(p->left) <= 0) {
+                rotateRight(p);
+            } else {
+                rotateLeftRight(p);
+            }
+        } else if (b == 2) { // right heavy
+            if (getBalance(p->right) >= 0) {
+                rotateLeft(p);
+            } else {
+                rotateRightLeft(p);
+            }
+        }
     }
 };
--- computational-geometry/LineIntersection/avl_test.cpp
+++ computational-geometry/LineIntersection/avl_test.cpp
@@ -1,21 +1,97 @@
 #include "AVLTree.cpp"
+#include <cassert>
 #include <fstream>
 #include <iostream>
+#include <random>
 
-const int nums[] = {1, 2, 5, 33, 57, 49, 11, 12, 7};
+#define CHECK(root, expr)                                                      \
+    do {                                                                       \
+        if (!(expr)) {                                                         \
+            std::cerr << "CHECK failed:" << #expr << " at " << __FILE__ << ":" \
+                      << __LINE__ << std::endl;                                \
+            if (root) {                                                        \
+                std::ofstream ofs("tree.dot");                                 \
+                ofs << root->toDot();                                          \
+                ofs.close();                                                   \
+                std::cout << "Dot writtent to tree.dot" << std::endl;          \
+            }                                                                  \
+            std::abort();                                                      \
+        }                                                                      \
+    } while (0)
+
+// pseudo-random ints
+const int nums[] = {413, 87,  589, 221, 774, 9,   669, 805, 144, 963, 352,
+                    291, 827, 462, 68,  936, 112, 647, 306, 745, 428, 93,
+                    577, 216, 781, 24,  688, 796, 132, 951, 339, 273, 818,
+                    449, 62,  928, 107, 654, 312, 739, 422, 81,  561, 232,
+                    769, 17,  660, 802, 140, 958, 345, 288, 823, 466, 74};
 
 int main() {
-    AVLNode<int, int> *t = new AVLNode<int, int>(0, 0);
-    for (const int num : nums) {
-        t = t->insert(num, num);
+    { // Test 1: Simple inserts
+        Node<int, int> *root = nullptr;
+        for (int i = 1; i <= 50; ++i) {
+            bool inserted = Node<int, int>::insert(root, i, i);
+            CHECK(root, inserted);
+            CHECK(root, (Node<int, int>::validate(root)));
+        }
+        CHECK(root, !(Node<int, int>::insert(root, 10, 10)));
+        CHECK(root, (Node<int, int>::validate(root)));
     }
 
-    // write dot to tree.dot
-    const std::string dot = t->toDot();
-    std::ofstream out("tree.dot");
-    if (!out) { // failed to open file
-        return 1;
+    { // Test 2: Random inserts and removals
+        Node<int, int> *root = nullptr;
+        std::mt19937 rng = std::mt19937(12345);
+        std::uniform_int_distribution<int> dist =
+            std::uniform_int_distribution<int>(1, 1000);
+        std::vector<int> insertedNums;
+        for (int i = 0; i <= 10; ++i) {
+            int k = dist(rng);
+            const int inserted = Node<int, int>::insert(root, k, k);
+            if (inserted) {
+                insertedNums.push_back(k);
+            }
+            CHECK(root, (Node<int, int>::validate(root)));
+        }
+
+        // remove half of the inserted keys
+        std::shuffle(insertedNums.begin(), insertedNums.end(), rng);
+        int numToRemove = insertedNums.size() / 2;
+        for (int i = 0; i < numToRemove; ++i) {
+            bool removed = Node<int, int>::remove(root, insertedNums[i]);
+            CHECK(root, removed);
+            CHECK(root, (Node<int, int>::validate(root)));
+        }
+        // Attemp removing non-existing key
+        CHECK(root, !(Node<int, int>::remove(root, -999)));
+        CHECK(root, (Node<int, int>::validate(root)));
     }
-    out << dot;
+    { // Test 3: Worst-case pattern (descending inserts)
+        Node<int, int> *root = nullptr;
+        for (int i = 1000; i >= 1; --i) {
+            Node<int, int>::insert(root, i, i);
+            CHECK(root, (Node<int, int>::validate(root)));
+        }
+        // Remove ascending
+        for (int i = 1; i <= 1000; ++i) {
+            Node<int, int>::remove(root, i);
+            CHECK(root, (Node<int, int>::validate(root)));
+        }
+        // Tree should be empty now (root == nullptr)
+        CHECK(root, root == nullptr);
+    }
+    std::cout << "All AVL tests passed." << std::endl;
+
+    { // print tree
+        Node<int, int> *root = nullptr;
+        for (int k : nums) {
+            Node<int, int>::insert(root, k, k);
+        }
+        CHECK(root, (Node<int, int>::validate(root)));
+        std::ofstream ofs("tree.dot");
+        ofs << root->toDot();
+        ofs.close();
+        std::cout << "Dot writtent to tree.dot" << std::endl;
+    }
+
     return 0;
 }
